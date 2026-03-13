1853def4f22f30ece54014c31f1fa36d42eb7556
homestech-hub <homestech@digicheap.net>
2026-03-13 09:11:48 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'homestech-v1';
+const urlsToCache = [
+  'mobile.html',
+  'https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700;800&display=swap',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
